9d4deef9a33dcb085eac457cae3a595a6a3524df
Igor Khripunov <ihripunov@gmail.com>
2024-04-16 21:41:28 +1200

    Tidy up openapi generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
     kotlin("plugin.jpa") version "1.9.23"
-    kotlin("kapt") version "1.9.23"
     id("org.openapi.generator") version "6.6.0"
 }
 
@@ -22,7 +21,7 @@ java {
 }
 
 openApiGenerate {
-    generatorName.set("spring")
+    generatorName.set("kotlin-spring")
     inputSpec.set("$rootDir/api-spec.yml")
     outputDir.set("$buildDir/generated")
     apiPackage.set("com.example.api")
@@ -37,18 +36,27 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.swagger.core.v3:swagger-annotations:2.2.21")
+    implementation("io.swagger.core.v3:swagger-core:2.2.21")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("jakarta.validation:jakarta.validation-api:3.0.2")
     implementation("org.openapitools:jackson-databind-nullable:0.2.6")
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
-    kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("com.h2database:h2")
 }
+java.sourceSets["main"].java.srcDir("$buildDir/generated/src/main/kotlin")
 
-java.sourceSets["main"].java.srcDir("$buildDir/generated/src/main/java")
 java.sourceSets["main"].resources.srcDir("$buildDir/generated/src/main/resources")
+
+tasks.compileKotlin {
+    dependsOn("openApiGenerate")
+}
+
+tasks.openApiGenerate {
+    dependsOn("processResources")
+}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
