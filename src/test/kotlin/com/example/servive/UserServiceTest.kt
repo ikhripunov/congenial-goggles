5756aa5c7466a59d4a1b8aac07a692bfb9fa4201
Igor Khripunov <ihripunov@gmail.com>
2024-04-17 20:43:32 +1200

    Improve unit test, introduce cases for multiple users, no users

--- src/test/kotlin/com/example/servive/UserServiceTest.kt
+++ src/test/kotlin/com/example/servive/UserServiceTest.kt
@@ -4,6 +4,7 @@ package com.example.servive
 import com.example.entity.User
 import com.example.repository.UserRepository
 import com.example.service.UserService
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
 import java.util.*
@@ -13,14 +14,56 @@ class UserServiceTest {
     val repo = mock(UserRepository::class.java)
     private val userService: UserService = UserService(repo)
 
+    @BeforeEach
+    fun setUp() {
+        clearInvocations(repo)
+    }
+
     @Test
-    fun basicTest() {
+    fun findUserByLastNameTest() {
+        // Given
         val user = User("foo", "bar", "baz@taz.com", UUID.randomUUID())
-        `when`( repo.findBySurname("foo")).thenReturn(listOf(user).toList())
-        val findUser = userService.findUser("foo")
-        assert(findUser.isNotEmpty())
-        assert(findUser[0].lastName.equals("foo"))
-        assert(findUser[0].firstName.equals("bar"))
-        assert(findUser[0].email.equals("baz@taz.com"))
+        `when`( repo.findByLastName("foo")).thenReturn(listOf(user))
+
+        // When
+        val findUserResult = userService.findUserByLastName("foo")
+
+        // Then
+        assert(findUserResult.isNotEmpty())
+        assert(findUserResult[0].lastName.equals("foo"))
+        assert(findUserResult[0].firstName.equals("bar"))
+        assert(findUserResult[0].email.equals("baz@taz.com"))
+    }
+
+    @Test
+    fun findManyUsersByLastNameTest() {
+        //Given
+        val user1 = User("foo", "bar", "baz@taz.com", UUID.randomUUID())
+        val user2 = User("foo", "bar2", "baz2@taz.com", UUID.randomUUID())
+        `when`( repo.findByLastName("foo")).thenReturn(listOf(user1, user2))
+
+        // When
+        val findUserResult = userService.findUserByLastName("foo")
+
+        // Then
+        assert(findUserResult.size == 2)
+        assert(findUserResult[0].lastName.equals("foo"))
+        assert(findUserResult[0].firstName.equals("bar"))
+        assert(findUserResult[0].email.equals("baz@taz.com"))
+        assert(findUserResult[1].lastName.equals("foo"))
+        assert(findUserResult[1].firstName.equals("bar2"))
+        assert(findUserResult[1].email.equals("baz2@taz.com"))
+    }
+
+    @Test
+    fun findNoUserUsersByLastNameTest() {
+        //Given
+        `when`( repo.findByLastName("foo")).thenReturn(listOf())
+
+        // When
+        val findUserResult = userService.findUserByLastName("foo")
+
+        // Then
+        assert(findUserResult.isEmpty())
     }
 }
