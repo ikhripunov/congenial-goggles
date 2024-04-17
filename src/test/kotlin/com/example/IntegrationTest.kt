539497fc86532ae5bb992a7f62f5750b81cff241
Igor Khripunov <ihripunov@gmail.com>
2024-04-17 22:49:54 +1200

    Rework integration tests, update api-spec to reflect desired behaviour

--- src/test/kotlin/com/example/IntegrationTest.kt
+++ src/test/kotlin/com/example/IntegrationTest.kt
@@ -1,18 +1,22 @@
 package com.example
 
 import com.example.entity.User
+import com.example.model.UserDTO
 import com.example.repository.UserRepository
-import org.hamcrest.Matcher
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.hamcrest.Matchers
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
+import org.springframework.test.web.servlet.post
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,21 +27,78 @@ class IntegrationTest(
     val repo: UserRepository
 ) {
 
-    @Test
-    fun testSuccess() {
-        repo.save(User("foo", "bar", "baz@taz.com"))
+    val om = ObjectMapper()
 
-        mockMvc.get("/user?lastName=foo")
-            .andExpect { status { isOk() } }
-            .andExpect { content { contentType(MediaType.APPLICATION_JSON) } }
-            .andExpect { jsonPath("[0].surname", Matchers.equalTo("foo")) }
+    @BeforeEach
+    fun setUp() {
+        repo.deleteAll()
     }
 
-    @Test
-    fun test400Failure() {
-        repo.save(User("foo", "bar", "baz@taz.com"))
+    @ParameterizedTest
+    @MethodSource("createUserInputsAndResults")
+    fun createUserTest(userDTO: UserDTO?, status: Int) {
+        // Given the userDTO
+        // When
+        mockMvc.post("/v1/users") {
+            contentType = MediaType.APPLICATION_JSON
+            content = om.writeValueAsString(userDTO)
+        }
+        // Then
+            .andExpect { status { isEqualTo(status) } }
+    }
 
-        mockMvc.get("/user?lastName=")
-            .andExpect { status { isBadRequest() } }
+    @ParameterizedTest
+    @MethodSource("findUserByLastNameInputsAndResults")
+    fun findUserByLastNameTest(usersInDb: List<User>, lastName: String, numberFound: Int, status: Int,) {
+        // Given
+        repo.saveAll(usersInDb)
+
+        // When
+        mockMvc.get("/v1/users?lastName=$lastName")
+            .andExpect { status { isEqualTo(status) } }
+            .andExpect { content { contentType(MediaType.APPLICATION_JSON) } }
+            .andExpect { jsonPath("$.length()", Matchers.equalTo(numberFound)) }
+    }
+
+    companion object {
+        @JvmStatic
+        fun createUserInputsAndResults(): List<Arguments> {
+            return listOf(
+                Arguments.of(UserDTO("bar").firstName("one").email("buzz"), 204),
+                Arguments.of(UserDTO().firstName("two").email("buzz"), 400),
+                Arguments.of(UserDTO("").firstName("three").email("buzz"), 400),
+                Arguments.of(null, 400)
+            )
+        }
+
+        @JvmStatic
+        fun findUserByLastNameInputsAndResults(): List<Arguments> {
+            return listOf(
+                Arguments.of(
+                    listOf(User("foo", "bar", "buzz"), User("foo", "bar", "buzz")),
+                    "foo",
+                    2,
+                    200
+                ),
+                Arguments.of(
+                    listOf(User("foo", "bar", "buzz"), User("bar", "bar", "buzz")),
+                    "foo",
+                    1,
+                    200
+                ),
+                Arguments.of(
+                    listOf(User("buzz", "bar", "buzz"), User("bar", "bar", "buzz")),
+                    "foo",
+                    0,
+                    200
+                ),
+                Arguments.of(
+                    listOf(User("buzz", "bar", "buzz"), User("bar", "bar", "buzz")),
+                    "",
+                    1,
+                    400
+                ),
+            )
+        }
     }
 }
